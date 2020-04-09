084fbeba14ae0102fd0ec8f8a75b76977c31a03a
RenilsonMedeiros <renilsonsilva@aluno.uema.br>
2020-04-09 15:45:35 -0300

    frontend da app be-the-hero
    Não consegui criar um react app pelo terminal, então usei o frontend da app passado. agora só preciso apagar o que não vou precisar para essa app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+//Um COMPONENTE no react: é uma função que retorna HTML que pode ter comandos JS e CSS.
+//JSX (JavaScript e XML): É o HTML dentro do JS
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Estou renderizando o meu componente App dentro da minha div com id= 'root'
+ReactDOM.render(<App />, document.getElementById('root')); 
